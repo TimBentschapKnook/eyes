7843f558bd9f0ef11d59c778083504f423d1def7
TimBentschapKnook <timbknook@gmail.com>
2020-09-25 06:28:38 +0200

    add navigation and shoppingcart

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
 
     // ONLICK ARROW SLIDESHOW HOME FULLSCREEN SCROLL DOWN
 	$('.slide-down-image').on('click', function() {
-        const scrollAmount = $('header').height();
+		console.log("click");
+		const scrollAmount = $('header').height();
+		console.log(scrollAmount);
 		$('html, body').animate(
 			{
 				scrollTop: scrollAmount,
@@ -20,7 +22,7 @@ $(document).ready(function() {
 			let scrollTo = scrollHeight;
 
 			if (scrollPos < scrollHeight) {
-				if ((scrollHeight / 4) * 1 > scrollPos) {
+				if ((scrollHeight / 5) * 1 > scrollPos) {
 					scrollTo = 0;
 				}
 				$('html, body').animate(
@@ -38,6 +40,7 @@ $(document).ready(function() {
 		clearTimeout($.data(this, 'scrollTimer'));
 		$.data(this, 'scrollTimer', setTimeout(function() {
 			onScrollStop();
+			console.log('timer');
 		}, 250));
 	});
 
