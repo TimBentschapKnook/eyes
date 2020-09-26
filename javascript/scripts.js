5c5608b0e38c759b582d1c9d9a053523f8a93413
TimBentschapKnook <timbknook@gmail.com>
2020-09-26 10:45:28 +0200

    made shoppingbasket

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
 
     // ONLICK ARROW SLIDESHOW HOME FULLSCREEN SCROLL DOWN
-	$('.slide-down-image').on('click', function() {
-		console.log("click");
-		const scrollAmount = $('header').height();
-		console.log(scrollAmount);
+	$('#slide-down').on('click', function() {
+        const scrollAmount = $('header').height();
 		$('html, body').animate(
 			{
 				scrollTop: scrollAmount,
@@ -13,36 +11,48 @@ $(document).ready(function() {
 		);
 	});
 
-	// Scroll back to top or bottom header
-	function onScrollStop() {
-		const scrollPos = $(window).scrollTop();
-
-		if ($('header').length > 0) {
-			const scrollHeight = $('header').height();
-			let scrollTo = scrollHeight;
-
-			if (scrollPos < scrollHeight) {
-				if ((scrollHeight / 5) * 1 > scrollPos) {
-					scrollTo = 0;
-				}
-				$('html, body').animate(
-					{
-						scrollTop: scrollTo,
-					},
-					500,
-				);
-			}
+	$('#showSpecs').click(function() {
+		$('#specs').toggleClass('showSpecs');
+		if($('#specs').hasClass('showSpecs')) {
+			$('#rotate').addClass('rotate');
+		} else {
+			$('#rotate').removeClass('rotate');
 		}
-	}
-
-	// Check on page if scrolled 
-	$(window).scroll(function() {
-		clearTimeout($.data(this, 'scrollTimer'));
-		$.data(this, 'scrollTimer', setTimeout(function() {
-			onScrollStop();
-			console.log('timer');
-		}, 250));
 	});
+
+	$('.nav-icon').click(function() {
+		$(this).toggleClass('open');
+	});
+
+	// // Scroll back to top or bottom header
+	// function onScrollStop() {
+	// 	const scrollPos = $(window).scrollTop();
+
+	// 	if ($('header').length > 0) {
+	// 		const scrollHeight = $('header').height();
+	// 		let scrollTo = scrollHeight;
+
+	// 		if (scrollPos < scrollHeight) {
+	// 			if ((scrollHeight / 4) * 1 > scrollPos) {
+	// 				scrollTo = 0;
+	// 			}
+	// 			$('html, body').animate(
+	// 				{
+	// 					scrollTop: scrollTo,
+	// 				},
+	// 				500,
+	// 			);
+	// 		}
+	// 	}
+	// }
+
+	// // Check on page if scrolled 
+	// $(window).scroll(function() {
+	// 	clearTimeout($.data(this, 'scrollTimer'));
+	// 	$.data(this, 'scrollTimer', setTimeout(function() {
+	// 		onScrollStop();
+	// 	}, 250));
+	// });
 
 	function onScroll() {
 		const menu = $('.menu');
