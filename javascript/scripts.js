2e6937387103040314f2cd803676c2192e7e94db
TimBentschapKnook <timbknook@gmail.com>
2020-10-16 09:59:05 +0200

    added dashboard

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -144,6 +144,7 @@ $(document).ready(function() {
 
 	onScroll();
 	onScrollNot100VH();
+	addUserData();
 
 	// Window scrollfunctions, als er gescrolld wordt op de pagina, voer deze functions uit
 	$(window).on('scroll', onScroll);
@@ -176,6 +177,24 @@ $(document).ready(function() {
 			var selectedOption = $(this).find(':selected').val();
 			CalcShoppingBasket(selectedOption);
 		});
+	}
+
+	function addUserData() {
+			 // Random var
+			 var gebruikersnaam = "Jan Klaasen";
+			 var geboortedatum = "1990-01-01";
+			 var adres = "Hendriksstraat 1"
+			 var woonplaats = "Sint-Johannes";
+			 var mailadres = "Voorbeeld@test.nl";
+		 
+			 // array1 voor het zoeken naar de id's, array2 vult in de gevonden id's
+			 var array1=["gebruikersnaam", "geboortedatum", "adres", "woonplaats", "mailadres"];
+			 var array2=[gebruikersnaam, geboortedatum, adres, woonplaats, mailadres];
+		 
+			 // For loop voor het invullen van de gegevens
+			 for (i = 0; i < array1.length; i++) {
+				 document.getElementById(array1[i]).value = array2[i];
+			 }
 	}
 	
 });
